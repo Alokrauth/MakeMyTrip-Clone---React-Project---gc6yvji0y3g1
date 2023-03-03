56a977d89749e29f0575fc17d606debd68a41e76
Alokrauth <alokkumarrauth@gmail.com>
2023-03-03 13:26:49 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,58 @@
-import React from 'react';
-import '../styles/App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Flight from './Flight';
-import Hotel from './Hotel';
-import Train from './Train';
-import Checkout from './Checkout';
-import Login from './Login';
-import Signup from './Signup';
-import Header from './Header';
-import Home from './Home';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
- function App() {
+import "../styles/App.css";
+import Login from "./Login/Login";
+
+import Master from "./Flight/Master";
+import MasterTrain from "./Train/MasterTrain";
+import MasterHotel from "./Hotel/MasterHotel";
+import Header from "./Header/Header";
+import Checkout from "./CheckOut/Checkout";
+
+const App = () => {
   return (
-    <div className='App'>
-      <BrowserRouter>
-      <Header/>
+    // <BrowserRouter>
+    //   <div id='main'>
+    //     <Routes>
+    //       <Route
+    //         path='/Master'
+    //         element={
+    //           <Master modal2Open={modal2Open} setModal2Open={setModal2Open} />
+    //         }
+    //       />
+    //       <Route path='/' element={<Login />} />
+    //       <Route
+    //         path='/checkout'
+    //         element={
+    //           <Modals modal2Open={modal2Open} setModal2Open={setModal2Open} />
+    //         }
+    //       />
+    //       <Route
+    //         path='/hotels'
+    //         element={
+    //           <MasterHotel
+    //             modal2Open={modal2Open}
+    //             setModal2Open={setModal2Open}
+    //           />
+    //         }
+    //       />
+    //       <Route path='/trains' element={<MasterTrain />} />
+    //     </Routes>
+    //   </div>
+    // </BrowserRouter>
+    // <MasterHotel />
+    <Router>
+      <Header />
       <Routes>
-        <Route path='/' element={ <Home/> } />
-        <Route path='/flight' element={ <Flight/> } />
-        <Route path='/hotel' element={ <Hotel/> } />
-        <Route path='/train' element={ <Train/> } />
-        <Route path='checkout' element={ <Checkout/> } />
-        <Route path='/login' element={ <Login/> } />
-        <Route path='/login/signup' element={ <Signup/> } />
+        <Route exact path='/' element={<Master />} />
+        <Route exact path='/masterTrain' element={<MasterTrain />} />
+        <Route exact path='/masterHotel' element={<MasterHotel />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/checkout' element={<Checkout />} />
       </Routes>
-      </BrowserRouter>
-    </div>
+    </Router>
   );
-}
-export default App
+};
+
+export default App;
